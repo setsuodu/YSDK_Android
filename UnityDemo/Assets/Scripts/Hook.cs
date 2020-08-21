5265575d47fadf9be02629b84e5b9b287e74b437
setsuodu <339131001@qq.com>
2020-08-21 17:37:38 +0800

    Unity工程，用于导出
    Signed-off-by: setsuodu <339131001@qq.com>

---
+++ UnityDemo/Assets/Scripts/Hook.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hook : MonoBehaviour
+{
+    public void NativeCallback(string log)
+    {
+        Debug.Log("[NativeCallback]" + log);
+    }
+
+    public void Init()
+    {
+        Debug.Log("Click Init..");
+
+        AndroidJavaClass jc = new AndroidJavaClass("com.unity3d.player.UnityPlayer"); //Manifest中指定的启动页
+        AndroidJavaObject jo = jc.GetStatic<AndroidJavaObject>("currentActivity");
+        jo.Call("Init");
+    }
+
+    public void Login()
+    {
+        Debug.Log("Click Login..");
+
+        AndroidJavaClass jc = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        AndroidJavaObject jo = jc.GetStatic<AndroidJavaObject>("currentActivity");
+        jo.Call("Login");
+    }
+}
